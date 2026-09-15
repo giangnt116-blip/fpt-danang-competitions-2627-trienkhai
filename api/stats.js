a90e8188dcff3bb268adc990e0e17ee8b089fdc2
giangnt116-blip <giangnt116@fpt.edu.vn>
2026-09-15 09:48:54 +0700

    Allow cross-origin access to public stats API

--- api/stats.js
+++ api/stats.js
@@ -2,6 +2,13 @@ const APPS_SCRIPT_STATS_URL =
   "https://script.google.com/macros/s/AKfycbwBZM7zChf7Tu_g__S5mf2ky1DvwckXsdyq4zAyHgH0E5WVZgjtqc4ScUm7FLVz7sJ9/exec?action=public-stats";
 
 export default async function handler(req, res) {
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+if (req.method === 'OPTIONS') {
+  return res.status(204).end();
+}
   if (req.method !== "GET") {
     return res.status(405).json({
       success: false,
